38608752b734ec8289911884cf396eb6909f6275
Yoan <yoan.cesp@gmail.com>
2024-04-29 10:22:26 +0100

    add reactions, autorun and when usecase examples

--- src/store/NotesStore.js
+++ src/store/NotesStore.js
@@ -1,4 +1,4 @@
-import {makeAutoObservable} from 'mobx';
+import {autorun, makeAutoObservable, reaction, when} from 'mobx';
 import {nanoid} from 'nanoid';
 
 class NotesStore {
@@ -6,6 +6,33 @@ class NotesStore {
 
     constructor() {
         makeAutoObservable(this);
+        // print the number of notes and their text when the notes array changes
+        autorun(() => {
+            console.log(`There are ${this.notes.length} notes.`);
+            this.notes.forEach(note => {
+                console.log(`Note: ${note.text} (ID: ${note.id})`);
+            });
+        });
+
+        // show an alert when the number of notes exceeds 10
+        // the reactions defines a side effect that will be executed when the notes array changes
+        reaction(
+            () => this.notes.length,
+            length => {
+                if (length > 10) {
+                    alert('Limit reached!');
+                }
+            }
+        );
+
+        // listen to the changes matching with the expected condition
+        when(
+            () => this.notes.length > 0,
+            () => {
+                console.log('First note added!... saving notes to local storage');
+                localStorage.setItem('notes', JSON.stringify(this.notes));
+            }
+        );
     }
 
     addNote = (text) => {
